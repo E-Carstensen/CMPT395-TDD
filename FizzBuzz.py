68560202c2ce2072c6b375f475752cfce814f95d
Ecars9 <ecarstensen99@gmail.com>
2023-02-02 16:21:53 -0700

    Added documentation

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,10 +1,18 @@
-
+''' Eric Carstensen - 3070801
+Takes an integer or a string numeric
+returns "Fizz" if input is divisable by 3
+        "Buzz" if input is divisable by 5
+        "FizzBuzz" if input is divisable by both 3 and 5
+        string input if divisable by neither
+        False if invalid input
+'''
 
 def fizzbuzz(i):
-    
+    #Check for invalid input
     if not str(i).isnumeric():
         return False
 
+    #Enforce type
     i=int(i)
     output = ""
 
--- password_validation.py
+++ password_validation.py
@@ -1,40 +1,45 @@
 
 '''
 Takes a potential password as a string and validates it against these criteria:
+
     Must be atleast 8 Chars long
     Must contain atleast 1 capital letter
     Must contain atleast 2 numbers
     Must contain atleast 1 special character
 
-
-
+If any of these criteria are not met it will return a string containing an error message for each missed criteria seperated by a \n
+If all criteria are met it will return True
 '''
 
 def validate(pswd):
 
     output = ""
 
+    #Enforce type
     if not isinstance(pswd, str):
         return False
 
     if len(pswd) < 8:
         output += "Password must be at least 8 characters\n"
     
+    #Counters for criteria
     nums = 0 
     caps = 0
     special = 0
 
     for char in pswd:
 
+        #If Char is a number
         if char.isnumeric():
             nums += 1
-
+        #If char is a special character
         elif char in '!@#$%^&*().,[]-=_+`~':
             special += 1
-
+        #If char is uppercase
         elif char.isupper():
             caps += 1
         
+    #If counter doesn't meet criteria, append error for each missed case
     if nums < 2:
         output += "The password must contain at least 2 numbers\n"
 
@@ -44,7 +49,9 @@ def validate(pswd):
     if special == 0:
         output += "password must contain at least one special character\n"
 
+    #If no errors are appended, return True
     if len(output) == 0:
         return True
     
+    #Else return errors
     return output
